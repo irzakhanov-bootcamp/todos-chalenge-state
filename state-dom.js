90cebfb6016cd15a6b773f72adafe1c0f8d55e15
Vakha <gvardee95@gmail.com>
2021-10-28 15:51:27 +0300

    Release 9. Refactor completed

--- state-dom.js
+++ state-dom.js
@@ -21,72 +21,85 @@ const todos = [
   },
 ];
 
+// Получаем узел кнопки добавления
 const addBtn = document.querySelector("#add-btn");
 
+//Объвляем функцию прорисовки новых тудушек
 const render = (todos) => {
-  const listContainer = document.querySelector("#list");
+  const listContainer = document.querySelector("#list"); //Получаем узел главного блока в котором будут храниться тудушки
 
+  // Проходимся помассиву тудушек переданных в эту функцию
   todos.forEach((todo, index) => {
-    const item = document.createElement("div");
-    const removeBtn = document.createElement("button");
-    const checkBox = document.createElement("input");
+    const item = document.createElement("div"); // Создаем новый узел для новой тудушки
+    const removeBtn = document.createElement("button"); // Создаем новый узел для кнопки удаления тудушки
+    const checkBox = document.createElement("input"); // Создаем новый узел input
 
-    checkBox.type = "checkbox";
+    checkBox.type = "checkbox"; // Делаем наш инпут типом checkbox
 
-    removeBtn.textContent = "X";
-    removeBtn.addEventListener("click", () => remove(index));
+    removeBtn.textContent = "X"; // Даем нашей кнопки удаления тудушки текст "X" типа крестик для удаления
+    removeBtn.addEventListener("click", () => remove(index)); // Вешаем обработчик события клика на кнопку удаления. Вызываем функцию удаления тудушки и передаем индекс данной ьудушки
 
-    item.classList.add("todo-list__item");
-    item.textContent = todo.text;
+    item.classList.add("todo-list__item"); // Даем класс для новой тудушки
+    item.textContent = todo.text; // Даем текст из объекта массива нашей новой тудушке
 
     if (todos[index].done) {
-      item.classList.add("completed");
-      checkBox.checked = true;
+      // Если в тудушке поле done равно true
+      item.classList.add("completed"); // Даем ему класс completed
+      checkBox.checked = true; // и чекбокс делаем с галочкой
     } else {
-      item.classList.remove("completed");
-      checkBox.checked = false;
+      // иначе
+      item.classList.remove("completed"); // Удаляем класс completed
+      checkBox.checked = false; // И убираем галочку с чекбокса
     }
 
-    item.append(checkBox);
-    item.append(removeBtn);
+    item.append(checkBox); // Добавляем наш новый чекбокс к тудушке
+    item.append(removeBtn); // и добавляем кнопку удаления к нашей тудушке
 
-    checkBox.onchange = () => checkTodo(index);
+    checkBox.onchange = () => checkTodo(index); // Вешаем обработик события нашему чекбоксу и вызываем функцию проверки тудушки на выполнение и передаем индекс данной тудушки
 
-    listContainer.append(item);
+    listContainer.append(item); // Добавляем наш новый туду в главный контейнер
   });
 };
 
+// Функция добавления тудушки
 const addTodo = (text) => {
+  // Пушик в наш массив новый объект с текстом который принимаем в качестве парраметра
   todos.push({
     text,
     done: false,
   });
-  document.querySelector("#list").textContent = "";
-  render(todos);
+
+  document.querySelector("#list").textContent = ""; // Очишаем экран от старой версии
+  render(todos); // рендерим заного
 };
 
+// Функция удаления тудушки
 const remove = (id) => {
-  todos.splice(id, 1);
+  todos.splice(id, 1); // Удаляем тудушку из массива по айдишнику который приняли в качестве параметра
 
-  document.querySelector("#list").textContent = "";
-  render(todos);
+  document.querySelector("#list").textContent = ""; // Очишаем экран от старой версии
+  render(todos); // рендерим заного
 };
 
+// Функция проверки тудушки на выполнение
 const checkTodo = (id) => {
-  todos[id].done = !todos[id].done;
+  todos[id].done = !todos[id].done; // Меняем значение ключа done на противоположное
 
-  document.querySelector("#list").textContent = "";
-  render(todos);
+  document.querySelector("#list").textContent = ""; // Очишаем экран от старой версии
+  render(todos); // рендерим заного
 };
 
 addBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const text = document.querySelector("#text").value;
+  // Вешаем событие клика на кнопку добавления тудушки
+  e.preventDefault(); // Отменяем стандартное поведение события
+  const text = document.querySelector("#text").value; // Получаем текст из нашего инпута
   if (text === "" || text === " ") {
-    return;
+    // Небольшая валидация
+    return; // Выходим из функции
   }
-  addTodo(text);
-  document.querySelector("#text").value = "";
+
+  addTodo(text); // Вызываем функцию добавления тудушки и передаем наш текст из инпута
+  document.querySelector("#text").value = ""; // Очишаем наш инпут
 });
 
-render(todos);
+render(todos); // Вызываем рендер
